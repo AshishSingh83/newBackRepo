9d7d31b4387a370dc9d2c505afb033c355e68418
Ashish Singh <ashishsingh822003@gmail.com>
2024-12-10 14:36:48 +0530

    ok

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const io = require("socket.io")(server, {
 });
 const corsOption = {
   credentials: true,
-  origin: ["http://localhost:3000"],
+  origin: ["myfrontendb.vercel.app"],
 };
 app.use(cors(corsOption));
 app.use("/storage", express.static("storage"));
